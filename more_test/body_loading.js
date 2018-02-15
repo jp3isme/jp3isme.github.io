a0ca72c20aa8e2a5ba2d27c5629c729d129d5b48
jp3isme <jp3isme@gmail.com>
2018-02-14 23:28:28 -0500

    test

--- more_test/body_loading.js
+++ more_test/body_loading.js
@@ -15,3 +15,35 @@ var movies;
 csv_content = document.getElementById("val_data").innerHTML;
 movies = $.csv.toArrays(csv_content);
 console.log(movies);
+
+var toAdd = document.createDocumentFragment();
+
+movies.forEach(function(el, index) {
+  var newDiv = document.createElement('div');
+  var link = document.createElement('a');
+  var img = document.createElement('img');
+  var h1 = document.createElement('h1');
+  var h3 = document.createElement('h3');
+  var h2 = document.createElement('h2');
+
+  var t1 = document.createTextNode(. . .);
+  var t3 = document.createTextNode(el[0]);
+  var t2 = document.createTextNode(el[1]);
+
+  h1.appendChild(t1);
+  h3.appendChild(t3);
+  h2.appendChild(t2);
+  newDiv.className = 'dates';
+  img.src = el[2];
+  link.href = el[3];
+
+  link.appendChild(img);
+  newDiv.appendChild(link);
+  newDiv.appendChild(h1);
+  newDiv.appendChild(h3);
+  newDiv.appendChild(h2);
+
+  toAdd.appendChild(newDiv);
+});
+
+$('.inner').append(toAdd);
