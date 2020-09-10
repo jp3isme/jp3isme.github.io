34d6084e5b3216edd97736c67669884017681765
jp3isme <jp3isme@gmail.com>
2020-09-10 00:16:50 -0400

    Updates

---
+++ precache-manifest.92c71d25b53da66ae6bd28bd0c63f103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01af281835f1d3cceb1c6cc15d088e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4eb21d26344fc53798",
+    "url": "/static/css/main.c633f229.chunk.css"
+  },
+  {
+    "revision": "74f429ccc4cdd1ad36be",
+    "url": "/static/js/2.408f6e09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.408f6e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4eb21d26344fc53798",
+    "url": "/static/js/main.932afbd1.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  }
+]);
