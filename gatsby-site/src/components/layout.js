901315804389896fe1faeef4952355f7b99fdf64
jp3isme <jp3isme@gmail.com>
2021-01-07 01:12:46 -0500

    moved some files

--- gatsby-site/src/components/layout.js
+++ gatsby-site/src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -27,6 +27,9 @@ const Layout = ({ children }) => {
   let toggleTheme = () => {
     setTheme(theme === lightTheme ? darkTheme : lightTheme)
   }
+
+  useEffect(() => {}, [theme])
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
