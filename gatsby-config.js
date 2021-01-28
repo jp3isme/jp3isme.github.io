d918a079cc3bc280b145975c9d4dba8066a0a011
jp3isme <jp3isme@gmail.com>
2021-01-28 02:07:08 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,35 +12,6 @@ module.exports = {
     twitterUsername: "@jp3isme",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/me_icon_white.PNG`, // This path is relative to the root of the site.
-        border_radius: "0.75rem",
-      },
-    },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
@@ -72,6 +43,35 @@ module.exports = {
         sampleRate: 5,
         siteSpeedSampleRate: 10,
         cookieDomain: "example.com",
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/me_icon_white.PNG`, // This path is relative to the root of the site.
+        border_radius: "0.75rem",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
