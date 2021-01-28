b88e4fff9cc4f7ad7e6273b4db1da49b22cea03c
jp3isme <jp3isme@gmail.com>
2021-01-28 02:22:16 -0500

    gatsby-plugin-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,10 +13,9 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingId: `${process.env.GOOGLE_TAG}`, // Google Analytics / GA
+        trackingId: `${process.env.GOOGLE_TAG}`,
         head: true,
         anonymize: true,
       },
