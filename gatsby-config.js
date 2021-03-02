56b131258a25890b04a8a7e9ec97a92c7bf9d45e
jp3isme <jp3isme@gmail.com>
2021-03-02 01:20:05 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/me_icon_white.PNG`, // This path is relative to the root of the site.
+        icon: `src/images/me_icon_white.png`, // This path is relative to the root of the site.
         border_radius: "0.75rem",
       },
     },
